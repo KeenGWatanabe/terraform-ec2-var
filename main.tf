323fe607017c3b637bc56b735c40e6118b16b8d5
KeenGWatanabe <mckeenweb@gmail.com>
2025-01-29 21:55:11 +0800

    r

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ locals {
 
 #declaring a aws_instance named "public"
 resource "aws_instance" "public" {
- ami                         = "ami-0df8c184d5f6ae949" #Challenge, find the AMI ID of Amazon Linux 2 in us-east-1
+ ami                         = "ami-0c614dee691cbbf37" #Challenge, find the AMI ID of Amazon Linux 2 in us-east-1
  instance_type               = "t2.micro"
- subnet_id                   = data.aws_subnet.existing_ce9_pub_subnet.id #vpc.tf data{}
+ subnet_id                   = data.aws_subnet.existing_ce9_pub_subnet.id #src vpc.tf data{}
  associate_public_ip_address = true
- key_name                    = "roger-tf-kp" #Change to your keyname
- vpc_security_group_ids = [aws_security_group.allow_ssh.id] #["sg-0885a93971d5ae801"]
+ key_name                    = "roger-debian-kp" #Change to your keyname
+ vpc_security_group_ids = [data.aws_security_group.existing_sg.id] #["sg-0885a93971d5ae801"]
  tags = {
    Name = "${ local.resource_prefix }-ec2-${ var.env }" # Ensure your 
  }
@@ -21,7 +21,7 @@ output "ec2_public_ip" {
 }
 
 
-# resource "aws_security_group" "allow_ssh" {
+# resource "aws_security_group" "rds_ssh" {
 #  name        = "${ local.resource_prefix }-sg" #-security-group-ssh
 #  description = "Allow SSH inbound"
 #  vpc_id      = data.aws_vpc.existing_ce9_vpc.id
@@ -29,7 +29,7 @@ output "ec2_public_ip" {
 
 
 # resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-#  security_group_id = aws_security_group.allow_ssh.id
+#  security_group_id = aws_security_group.rds_ssh.id
 #  cidr_ipv4         = "0.0.0.0/0" 
 #  from_port         = 22
 #  ip_protocol       = "tcp"
--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@
 data "aws_vpc" "existing_ce9_vpc" {
  filter {
    name   = "tag:Name"
-   values = ["ce9-coaching-shared-vpc"] #"roger-vpc2"
+   values = ["ce9-coaching-shared-vpc"] #"roger-vpc2" ""
  }
 }
 
@@ -15,8 +15,8 @@ data "aws_subnet" "existing_ce9_pub_subnet" {
    values = ["ce9-coaching-shared-vpc-public-us-east-1a"] #"roger-public-subnet-1a"
  }
 }
-
-data "aws_security_group" "allow_ssh" {
+#Fetch the existing security group
+data "aws_security_group" "existing_sg" {
   filter {
     name   = "group-name"
     values = ["roger-rds-sg"] #Replace with your security group name
